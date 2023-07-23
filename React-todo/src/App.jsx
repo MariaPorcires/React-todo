33f24f62a0d938c5674a49d7ba41e2fb0d782913
hellnomary <maria.porcires@outlook.com>
2023-07-23 09:26:13 +0200

    setup

---
+++ React-todo/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, todo: "Study", time: 1100},
+    {id: 2, todo: "Clean house", time: 1400},
+    {id: 3, todo: "Make dinner", time: 1700}
+  ])
+
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default App
+
+
+//lista, visa upp lista
